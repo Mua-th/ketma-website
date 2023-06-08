bd5b1da5ba756bca443eb11e4741d715ca429089
Mua-th <124093480+Mua-th@users.noreply.github.com>
2023-06-09 01:43:34 +0200

    Initial commit

---
+++ challengejs.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+// Get the parent element
+var parentElement = document.getElementById('animated'); // Replace 'parentElementId' with the actual ID of the parent element
+
+let ket = document.createElement("span");
+ket.innerHTML="KETMA"
+ket.setAttribute("class","logosp");
+parentElement.appendChild(ket);
+
+
+// Append the new element to the parent element
+for (let i =0 ; i<2;i++){
+   
+// Create the new element
+var newElement = document.createElement('img');
+parentElement.prepend(newElement);
+
+// Add content to the new element if needed
+newElement.src="kesl.png";
+newElement.alt="";
+newElement.id="anim"+i;
+newElement.style="margin : -10px";
+}
+
+console.log("done");
+})
